498ccc071513d58fc54fa8638521b0ba946e9de4
Sandeep Gangarapu <sandeep_gangarapu@apple.com>
2022-12-01 18:53:11 -0800

    adding ipnb of both day 1 and 2

--- day1.py
+++ day1.py
@@ -1,10 +1,2 @@
-import numpy as np
-import pandas as pd
 
-data = pd.read_csv('input_files/day1.txt', header=None, skip_blank_lines=False)
-lis = np.array(data[0])
-missing_val_indices = np.where(np.isnan(lis))[0]
-split_lis = np.split(lis, missing_val_indices)
-out = list(map(np.nansum, split_lis))
-out = np.sort(np.array(out))[::-1][0:3]
-print(np.sum(out))
+
